5c1b83dc62e842437fd603a0c15fa4048f72400e
Oksana Tozhovez <tozhovez@gmail.com>
2022-06-27 10:05:03 +0300

    add init-hive-metastor

--- dockers/hive-metastore-pg/init-hive-db.sh
+++ dockers/hive-metastore-pg/init-hive-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+POSTGRES_USER=admin
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
@@ -8,10 +8,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
   \c metastore
 
-  \i /hive/hive-schema-2.3.0.postgres.sql
-  \i /hive/hive-txn-schema-2.3.0.postgres.sql
-  \i /hive/upgrade-2.3.0-to-3.0.0.postgres.sql
-  \i /hive/upgrade-3.0.0-to-3.1.0.postgres.sql
+  \i /hive-schema-2.3.0.postgres.sql
+  \i /hive-txn-schema-2.3.0.postgres.sql
+  \i /upgrade-2.3.0-to-3.0.0.postgres.sql
+  \i /upgrade-3.0.0-to-3.1.0.postgres.sql
 
   \pset tuples_only
   \o /tmp/grant-privs
